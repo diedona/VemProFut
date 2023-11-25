039ae9792df338c2a10d66f0234137d6c2618056
diedona <diedona@gmail.com>
2023-11-25 09:42:39 -0300

    add program.services.cs

---
+++ src/Api/VemProFut.Api/Program.Services.cs
@@ -0,0 +1,15 @@
+﻿using Carter;
+
+namespace VemProFut.Api
+{
+    public static class Program
+    {
+        public static void AddCustomServices(this IServiceCollection services)
+        {
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+            services.AddCarter();
+            services.AddMediatR(configuration => configuration.RegisterServicesFromAssembly(typeof(Program).Assembly));
+        }
+    }
+}
--- src/Api/VemProFut.Api/Program.cs
+++ src/Api/VemProFut.Api/Program.cs
@@ -1,14 +1,11 @@
 using Carter;
+using VemProFut.Api;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddCarter();
-builder.Services.AddMediatR(configuration => 
-    configuration.RegisterServicesFromAssembly(typeof(Program).Assembly));
+builder.Services.AddCustomServices();
 
 var app = builder.Build();
 
